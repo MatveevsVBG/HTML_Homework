873555a2274187b5977bea3f81acef818f85b07b
Sergei Matveev <msergne@gmail.com>
2023-04-23 22:37:11 +0300

    Add поле ввода email

---
+++ My-site/scripts/script.js
@@ -0,0 +1,8 @@
+function getEmail(){
+    var input = document.getElementById("input_email").value;
+    if (input ===""){
+        alert("Вы не ввели почту")
+    } else {
+        alert("Данные отправлены")
+    }
+}
